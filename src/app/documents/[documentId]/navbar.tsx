dbab449d8bdef04f2d621193fb877a5cb47bf510
MAULIK BHAVNAGARIYA <bhavnagariyamaulik@gmail.com>
2025-04-02 09:55:06 +0530

    Add Navbar component and integrate it into the document page layout

---
+++ src/app/documents/[documentId]/navbar.tsx
@@ -0,0 +1,155 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import DocumentInput from './document-input'
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarSub, MenubarSubContent, MenubarSubTrigger, MenubarTrigger } from '@/components/ui/menubar'
+import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlus, FilePlusIcon, FileTextIcon, GlobeIcon, ItalicIcon, Menu, PrinterIcon, Redo2Icon, RemoveFormattingIcon, StrikethroughIcon, TableIcon, TextIcon, TrashIcon, UnderlineIcon, Undo2Icon } from 'lucide-react'
+import { BsFilePdf } from 'react-icons/bs'
+
+const Navbar = () => {
+    return (
+        <nav className='flex items-center justify-between'>
+            <div className='flex gap-2 items-center'>
+                <Link href={"/"}>
+                    <Image src="/logo.svg" alt="logo" width={36} height={36} />
+                </Link>
+                <div className='flex flex-col'>
+                    <DocumentInput />
+                    <div className='flex flex-col'>
+                        <Menubar className='border-none bg-transparent shadow-none h-auto p-0'>
+                            <MenubarMenu>
+                                <MenubarTrigger className='text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto'>
+                                    File
+                                </MenubarTrigger>
+                                <MenubarContent className='print:hidden'>
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            <FileIcon className='mr-2 h-4 w-4' />
+                                            Save
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                <FileJsonIcon className='mr-2 h-4 w-4' />
+                                                JSON
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <GlobeIcon className='mr-2 h-4 w-4' />
+                                                HTML
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <BsFilePdf className='mr-2 h-4 w-4' />
+                                                PDF
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <FileTextIcon className='mr-2 h-4 w-4' />
+                                                Text
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                    <MenubarItem>
+                                        <FilePlusIcon className='mr-2 h-4 w-4' />
+                                        New Document
+                                    </MenubarItem>
+                                    <MenubarSeparator />
+                                    <MenubarItem>
+                                        <FilePenIcon className='mr-2 h-4 w-4' />
+                                        Rename
+                                    </MenubarItem>
+                                    <MenubarItem>
+                                        <TrashIcon className='mr-2 h-4 w-4' />
+                                        Remove
+                                    </MenubarItem>
+                                    <MenubarSeparator />
+                                    <MenubarItem onClick={() => window.print()}>
+                                        <PrinterIcon className='mr-2 h-4 w-4' />
+                                        Print<MenubarShortcut>⌘P</MenubarShortcut>
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className='text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto'>
+                                    Edit
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem>
+                                        <Undo2Icon className='mr-2 h-4 w-4' />
+                                        Undo<MenubarShortcut>⌘Z</MenubarShortcut>
+                                    </MenubarItem>
+                                    <MenubarItem>
+                                        <Redo2Icon className='mr-2 h-4 w-4' />
+                                        Redo<MenubarShortcut>⌘Y</MenubarShortcut>
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className='text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto'>
+                                    Insert
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            <TableIcon className='mr-2 h-4 w-4' />
+                                            Table
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                1 x 1
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                2 x 2
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                3 x 3
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                4 x 4
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className='text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto'>
+                                    Format
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarSub>
+                                        <MenubarSubTrigger>
+                                            <TextIcon className='mr-2 h-4 w-4' />
+                                            Text
+                                        </MenubarSubTrigger>
+                                        <MenubarSubContent>
+                                            <MenubarItem>
+                                                <BoldIcon className='mr-2 h-4 w-4' />
+                                                Bold<MenubarShortcut>⌘B</MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <ItalicIcon className='mr-2 h-4 w-4' />
+                                                Italic<MenubarShortcut>⌘I</MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <UnderlineIcon className='mr-2 h-4 w-4' />
+                                                Underline<MenubarShortcut>⌘U</MenubarShortcut>
+                                            </MenubarItem>
+                                            <MenubarItem>
+                                                <StrikethroughIcon className='mr-2 h-4 w-4' />
+                                                <span>Strikethrough&nbsp;&nbsp;</span><MenubarShortcut>⌘S</MenubarShortcut>
+                                            </MenubarItem>
+                                        </MenubarSubContent>
+                                    </MenubarSub>
+                                    <MenubarItem>
+                                        <RemoveFormattingIcon className='mr-2 h-4 w-4' />
+                                        Clear Formatting
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                        </Menubar>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Editor from './editor'
 import Toolbar from './toolbar'
+import Navbar from './navbar'
 
 interface pageProps {
     params: Promise<{ documentId: string }>
@@ -10,8 +11,13 @@ const page = async ({ params }: pageProps) => {
     const { documentId } = await params
     return (
         <div className='min-h-screen bg-[#FAFBFD]'>
-            <Toolbar />
-            <Editor />
+            <div className='flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden'>
+                <Navbar />
+                <Toolbar />
+            </div>
+            <div className='pt-[114px] print:pt-0'>
+                <Editor />
+            </div>
         </div>
     )
 }
